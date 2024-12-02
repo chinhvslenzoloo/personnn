9de5aaf37c56e46444d24a4d9a0c5ac38e44e814
chinhvslenzoloo <chinhvslenzoloo@gmail.com>
2024-12-01 23:55:55 -0800

    Add files via upload

---
+++ za svvliin person/script.js
@@ -0,0 +1,62 @@
+let words = document.querySelectorAll(".word");
+words.forEach((word) => {
+    let letters = word.textContent.split("");
+    word.textContent = "";
+    letters.forEach((letter) => {
+        let span = document.createElement("span");
+        span.textContent = letter;
+        span.className = "letter";
+        word.append(span);
+    });
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
+
+let changeText = () => {
+    let currentWord = words[currentWordIndex];
+    let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+    Array.from(currentWord.children).forEach((letter, i) => {
+        setTimeout(() => {
+            letter.className = "letter out";
+        }, i * 80);
+    });
+
+    nextWord.style.opacity = "1";
+    Array.from(nextWord.children).forEach((letter, i) => {
+        letter.className = "letter behind";
+        setTimeout(() => {
+            letter.className = "letter in";
+        }, 340 + i * 80);
+    });
+
+    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+
+changeText();
+setInterval(changeText, 3000);
+
+
+const circles = document.querySelectorAll('.circle');
+
+circles.forEach(elem => {
+    const dots = parseInt(elem.getAttribute("data-dots")); // Цэгийн нийт тоо
+    const market = parseInt(elem.getAttribute("data-percent")); // Хувь
+    const percent = Math.floor(dots * market / 100); // Хувиар тооцоолох
+    let points = ""; // Дугуй үүсгэх HTML
+    const rotate = 360 / dots; // Эргэлтийн өнцөг
+
+    // Дугуйг үүсгэх
+    for (let i = 0; i < dots; i++) {
+        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+    elem.innerHTML = points;
+
+    // Идэвхжүүлсэн цэгүүдийг тодруулах
+    const pointsMarked = elem.querySelectorAll('.points');
+    for (let i = 0; i < percent; i++) {
+        pointsMarked[i].classList.add('marked');
+    }
+});
